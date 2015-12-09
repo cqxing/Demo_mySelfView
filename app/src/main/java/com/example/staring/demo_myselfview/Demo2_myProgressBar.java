d6ade5e49aca04b009bd219543d4d920f5b2bdce
staringchen <15088132565@163.com>
2015-12-09 13:15:32 +0800

    第二次提交 测试

---
+++ app/src/main/java/com/example/staring/demo_myselfview/Demo2_myProgressBar.java
@@ -0,0 +1,14 @@
+package com.example.staring.demo_myselfview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+/**
+ * Created by staring on 2015/12/9.
+ */
+public class Demo2_myProgressBar extends ProgressBar {
+    public Demo2_myProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
